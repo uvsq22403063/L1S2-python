b94e935cbc3bd2d4a1be5db3e9a9b70556dfc618
Gaston Barthalon <gaston.barthalon@ens.uvsq.fr>
2025-02-19 17:00:10 +0100

    s

--- TD/gui/example03.py
+++ TD/gui/example03.py
@@ -5,7 +5,7 @@ CANVAS_WIDTH, CANVAS_HEIGHT = 600, 400
 if __name__ == '__main__':
     root = Tk()
 
-    canvas = Canvas(root, width = CANVAS_WIDTH, height = CANVAS_HEIGHT)
+    canvas = Canvas(root, width=CANVAS_WIDTH, height=CANVAS_HEIGHT)
 
     # Début de votre code
     x0 = 100
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     canvas.create_oval(x0 - 50, y + 50, x0 + 50, y - 50)
     canvas.create_oval(x1 - 50, y + 50, x1 + 50, y - 50)
     canvas.create_oval((x0 + x1) / 2 - 50, y + 50, (x0 + x1) / 2 + 50, y - 50)
-    
+
     # Fin de votre code
 
     canvas.pack()
