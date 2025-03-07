7c7e0952ab53306b83c0c365c1742f3382db177c
Gaston Barthalon <gaston.barthalon@ens.uvsq.fr>
2025-03-07 23:24:16 +0100

    fs

--- TD/TD2/dessin.py
+++ TD/TD2/dessin.py
@@ -4,32 +4,56 @@ import random as rd
 wd = 720
 he = 420
 color = "red"
+objets = []
 
 
 def cercle():
     x = rd.randint(20, wd - 20)
     y = rd.randint(20, he - 20)
 
-    canva.create_oval(x - 20, y - 20, x + 20, y + 20, fill=color)
+    cercle = canva.create_oval(x - 20, y - 20, x + 20, y + 20, fill=color)
+    objets.append(cercle)
+    print(objets)
 
 
 def carre():
     x = rd.randint(50, wd - 50)
     y = rd.randint(50, he - 50)
-    canva.create_rectangle(x - 20, y - 20, x + 20, y + 20, fill=color)
+    carre = canva.create_rectangle(x - 20, y - 20, x + 20, y + 20, fill=color)
+    objets.append(carre)
+    print(objets)
 
 
 def croix():
     x = rd.randint(50, wd - 50)
     y = rd.randint(50, he - 50)
 
-    canva.create_line(x, y, x + 20, y + 20, fill=color)
-    canva.create_line(x + 20, y, x, y + 20, fill=color)
+    ligne1 = canva.create_line(x, y, x + 20, y + 20, fill=color)
+    ligne2 = canva.create_line(x + 20, y, x, y + 20, fill=color)
+    objets.append(ligne1)
+    objets.append(ligne2)
+    print(objets)
 
 
 def colo():
     global color
     color = entree.get()
+
+
+def undo():
+
+    if len(objets) == 0:
+        nul = canva.create_rectangle(0, 0, 0, 0, fill="black")
+        objets.append(nul)
+
+    if canva.type(objets[-1]) == "line":
+
+        for i in range(2):
+            canva.delete(objets[-1])
+            objets.pop()
+    else:
+        canva.delete(objets[-1])
+        objets.pop()
 
 
 window = tk.Tk()
@@ -47,11 +71,14 @@ b3 = tk.Button(window, text="carré", padx=20, pady=1,
                             command=carre, bg="lightgray")
 b4 = tk.Button(window, text="croix", padx=20, pady=1,
                             command=croix, bg="lightgray")
+b5 = tk.Button(window, text="undo", padx=20, pady=1,
+                            command=undo, bg="lightgray")
 
-b1 = b1.grid(column=0, row=0)
-b2 = b2.grid(column=0, row=1)
-b3 = b3.grid(column=0, row=2)
-b4 = b4.grid(column=0, row=3)
+b1.grid(column=0, row=0)
+b2.grid(column=0, row=1)
+b3.grid(column=0, row=2)
+b4.grid(column=0, row=3)
+b5.grid(column=0, row=4)
 
 canva.grid(row=1, column=1, rowspan=3)
 window.mainloop()
